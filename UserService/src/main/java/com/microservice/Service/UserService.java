bbecb20b75c6db4eb7facd1c0c2d4b62a23d407d
MuggleLee <770796059@qq.com>
2019-07-21 00:47:05 +0800

    Add file to Github

---
+++ UserService/src/main/java/com/microservice/Service/UserService.java
@@ -0,0 +1,7 @@
+package com.microservice.Service;
+
+/**
+ * Created by hao on 2019/7/21.
+ */
+public interface UserService {
+}
---
+++ UserService/src/main/java/com/microservice/controller/UserConntroller.java
@@ -0,0 +1,15 @@
+package com.microservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hao on 2019/7/21.
+ */
+@Slf4j
+@RestController
+public class UserConntroller {
+
+
+
+}
